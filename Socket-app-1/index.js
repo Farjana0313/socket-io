35105d2b5c19cdd318e368650627e664a3e82819
Farjana0313 <farjanaakter178@gmail.com>
2024-05-09 12:51:13 +0600

    socket connected and text

--- Socket-app-1/index.js
+++ Socket-app-1/index.js
@@ -3,6 +3,18 @@ const app = express();
 const http = require('http');
 const expressServer = http.createServer(app);
 
+// Socket configure
+const { Server } = require('socket.io');
+const io = new Server(expressServer);
+
+io.on('connection', function(socket){
+    console.log('New user Connected');
+
+    socket.on('disconnect', function(){
+        console.log('User disconnected');
+    });
+})
+// clinet side configure
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html")
 })
